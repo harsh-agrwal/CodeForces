97f8d329455cc19f694d470fe6682c0357ce0635
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-05-27 11:12:20 +0530

    Create Domino piling.java

---
+++ Domino piling.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Dominoes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int M = scanner.nextInt();
+        int N = scanner.nextInt();
+
+        int maxDominoes = (M * N) / 2;
+
+        System.out.println(maxDominoes);
+
+        scanner.close();
+    }
+}
